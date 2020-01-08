a29204dbb22f3fab91c391af31b9e3bc0f17157f
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-08 19:09:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
    params[:number].to_i.times do
      answer += params[:phrase]
    end
+   binding.pry
    answer
   end
   get '/say/:word1/:word2/:word3/:word4/:word5' do
